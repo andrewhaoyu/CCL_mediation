2b80fba6960e2cfa6dddc566ebb52470efc85d98
Zhang <andrew.haoyu@gmail.com>
2023-06-05 16:09:27 -0400

    update

--- code/2_merge_model_result.R
+++ code/2_merge_model_result.R
@@ -7,7 +7,22 @@ for(i1 in 1:length(var_list)){
   load(paste0("./result/mediation_result_delta_",i1,".rdata"))
   result_list[[i1]] = result
 }
-final_result = rbindlist(result_list)
+final_result = cbind(var_list,rbindlist(result_list))
+colnames(final_result) = c("Mediator",
+                           "OR for Natural Director Effect (NDE)",
+                           "95% CI low for OR of NDE",
+                           "95% CI high for OR of NDE",
+                           "P-value for OR of NDE",
+                           "OR for Natural Indirector Effect (NIE)",
+                           "95% CI low for OR of NIE",
+                           "95% CI high for OR of NIE",
+                           "P-value for OR of NIE",
+                           "OR for Total Effect (TE)",
+                           "95% CI low for OR of TE",
+                           "95% CI high for OR of TE",
+                           "P-value for OR of TE",
+                           "Proportion of effect explained by indrect effect")
+write.csv(final_result, file = "./result/mediation_result_060523.csv")
 # library(mediation)
 # i1 = 6
 # load(paste0("./result/mediation_result_",i1,".rdata"))
